896a7092d9d6d321a90a48ee76b95ac78f31774b
Clara <claragabrii@gmail.com>
2026-02-11 16:39:36 -0300

    feat: adding message

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   getHello(): string {
-    return 'Welcome to GlobalCodePassport API!';
+    return 'Biel, fiz meu primeiro deploy!!!';
   }
 }
